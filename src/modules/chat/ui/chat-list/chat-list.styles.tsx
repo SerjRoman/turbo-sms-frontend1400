f3346751c198afcaa56be93b5eb71d8ac8af0dbd
David Petrenko <121516101+Davidptn@users.noreply.github.com>
2026-05-05 18:04:33 +0300

    chat-list

---
+++ src/modules/chat/ui/chat-list/chat-list.styles.tsx
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    listContainer: {
+        flex: 1,
+    },
+
+    empty: {
+        alignItems: 'center',
+        marginTop: 50,
+    }
+})
